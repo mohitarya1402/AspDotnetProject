82e9d1813104ba1215a944354ada0880eb443ef3
mohitarya1402 <68953842+mohitarya1402@users.noreply.github.com>
2020-09-03 15:30:27 +0530

    Add project files.

---
+++ AspDotnetProject/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using AspDotnetProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspDotnetProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        AspProjectEntities db = new AspProjectEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user_ objchk)
+        {
+            if(ModelState.IsValid)
+            {
+                using (AspProjectEntities db = new AspProjectEntities())
+                {
+                    var obj = db.user_.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if(obj != null)
+                    {
+                        Session["userID"] = obj.id.ToString();
+                        Session["userNAME"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", " The Username or Password is incorrect");
+                    }
+                }
+                  
+
+            }
+
+
+            return View(objchk);
+
+        }
+
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+
+    }
+}
---
+++ AspDotnetProject/Controllers/user_Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspDotnetProject.Models;
+
+namespace AspDotnetProject.Controllers
+{
+    public class user_Controller : Controller
+    {
+        private AspProjectEntities db = new AspProjectEntities();
+
+        // GET: user_
+        public ActionResult Index()
+        {
+            return View(db.user_.ToList());
+        }
+
+        // GET: user_/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_ user_ = db.user_.Find(id);
+            if (user_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_);
+        }
+
+        // GET: user_/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: user_/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,FirstName,lastName,username,password")] user_ user_)
+        {
+            if (ModelState.IsValid)
+            {
+                db.user_.Add(user_);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user_);
+        }
+
+        // GET: user_/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_ user_ = db.user_.Find(id);
+            if (user_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_);
+        }
+
+        // POST: user_/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,FirstName,lastName,username,password")] user_ user_)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_);
+        }
+
+        // GET: user_/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user_ user_ = db.user_.Find(id);
+            if (user_ == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_);
+        }
+
+        // POST: user_/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user_ user_ = db.user_.Find(id);
+            db.user_.Remove(user_);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
